3d2a6c6980e969a7efa820a7f5408381a5b67462
“Prabhakaran” <prabhakaran97dec@gmail.com>
2026-02-23 00:18:23 +0530

    4th stage

--- app-api/Src/helper/ConnectMongoDB.js
+++ app-api/Src/helper/ConnectMongoDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/grocery_mart');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('DB Name:', mongoose.connection.name);
   } catch (err) {
     console.error('DB Connection Error:', err);
